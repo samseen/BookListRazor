321cff14478b4e5403badd52fd7735d0b50e48a3
Samson Akanbi <samson.akanbi@outlook.com>
2020-10-16 19:46:26 +0100

    Implemented the delete functionality on the book object.

--- BookListRazor/Pages/BookList/Index.cshtml.cs
+++ BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -23,5 +23,19 @@ namespace BookListRazor.Pages.BookList
         {
             Books = await _context.Books.ToListAsync();
         }
+
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("Index");
+        }
     }
 }
