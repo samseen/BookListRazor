5ba6631f3c181c01572da1d5e9154028f5b33c95
Samson Akanbi <samson.akanbi@outlook.com>
2020-10-16 17:25:27 +0100

    Created the index page for the Book List.

---
+++ BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+        public async Task OnGet()
+        {
+            Books = await _context.Books.ToListAsync();
+        }
+    }
+}
