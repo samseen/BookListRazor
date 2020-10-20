5885173918599133107c78b8ca0a58a77cf8e600
Samson Akanbi <samson.akanbi@outlook.com>
2020-10-20 17:14:09 +0100

    Returned the list of books in the database as a json file in the book controller.

--- BookListRazor/Controllers/BookController.cs
+++ BookListRazor/Controllers/BookController.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookListRazor.Controllers
 {
     public class BookController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public BookController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            return Json(new { data = _context.Books.ToList() });
         }
     }
 }
